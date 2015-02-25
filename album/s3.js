64af89f8b31a7c8f090918e991d23c1bdbca2e6d
Burnside <burnside3@burnside3ml.llnl.gov>
2015-02-25 15:29:42 -0800

    Load the correct URL for images.

--- album/s3.js
+++ album/s3.js
@@ -50,6 +50,7 @@ var s3 = function () {
                 return;
             var contents = _.map(json.contents, function (item) {
                 item.path = this.get("path") + item.name;
+                item.url = "http://" + my.bucket() + "/" + item.path;
                 item.thumb = this.get("path") + item.name.replace(/.jpg/i,
                     function (m) { return ".thumb" + m; });
                 item.editable = false;
